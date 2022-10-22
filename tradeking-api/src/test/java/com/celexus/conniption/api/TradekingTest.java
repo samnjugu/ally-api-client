8c4e9233cdf5cd497ed93138d3200540a09b957f
samnjugu <samnjugu@gmail.com>
2022-10-22 19:33:30 -0400

    Added Jaxb dependencies to support Java 11

--- tradeking-api/src/test/java/com/celexus/conniption/api/TradekingTest.java
+++ tradeking-api/src/test/java/com/celexus/conniption/api/TradekingTest.java
@@ -1,2 +1,14 @@
-package com.celexus.conniption.api;public class TradekingTest {
+package com.celexus.conniption.api;
+
+import com.celexus.conniption.foreman.TradeKingForeman;
+import org.junit.Test;
+
+public class TradekingTest {
+
+    /* Test we can un/marshal accounts Object using our models**/
+    @Test
+    public void accountsTest(){
+        TradeKing tk = new TradeKing(new TradeKingForeman());
+        System.out.println(tk.accounts().getAccounts().toString());
+    }
 }
